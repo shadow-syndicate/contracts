342be9b64ffab9f9b91f5d8a706ce46c6d45eb66
KindeX <kindex@kindex.lv>
2021-12-12 08:56:33 +0200

    Roach NFT contract: genomes and birth cooldown

---
+++ contracts/Operators.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+// ***REMOVED***
+pragma solidity ^0.8.10;
+
+import "OpenZeppelin/openzeppelin-contracts@4.4.0/contracts/token/ERC20/IERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.4.0/contracts/access/Ownable.sol";
+
+contract Operators is Ownable {
+    mapping (address=>bool) operatorAddress;
+
+    modifier onlyOperator() {
+        require(isOperator(msg.sender), "Access denied");
+        _;
+    }
+
+    function isOwner(address _addr) public view returns (bool) {
+        return owner() == _addr;
+    }
+
+    function isOperator(address _addr) public view returns (bool) {
+        return operatorAddress[_addr] || isOwner(_addr);
+    }
+
+    function addOperator(address _newOperator) external onlyOwner {
+        require(_newOperator != address(0), "New operator is empty");
+
+        operatorAddress[_newOperator] = true;
+    }
+
+    function removeOperator(address _oldOperator) external onlyOwner {
+        delete(operatorAddress[_oldOperator]);
+    }
+
+    /**
+     * @dev Owner can claim any tokens that transferred
+     * to this contract address
+     */
+    function withdrawERC20(IERC20 _tokenContract, address _admin) external onlyOwner {
+        uint256 balance = _tokenContract.balanceOf(address(this));
+        _tokenContract.transfer(_admin, balance);
+    }
+}
--- contracts/RoachNFT.sol
+++ contracts/RoachNFT.sol
@@ -3,10 +3,68 @@
 pragma solidity ^0.8.10;
 
 import "OpenZeppelin/openzeppelin-contracts@4.4.0/contracts/token/ERC721/ERC721.sol";
+import "./Operators.sol";
 
-contract RoachNFT is ERC721 {
+type Genome is uint256;
+
+contract RoachNFT is ERC721, Operators {
+
+    struct Roach {
+        Genome genome;
+        uint40[2] parents;
+        uint40 creationTime;
+        uint40 birthTime;
+    }
+
+    Roach[] public roach;
+    // uint public BIRTH_COOLDOWN = uint256(-1); // max int, will be changed to 7 week later
+    uint public BIRTH_COOLDOWN = 1; // for debug only
+    uint constant public EMPTY_GENOME = 0;
+
+    event Birth(uint tokenId);
 
     constructor() ERC721('Roach Racing Club', 'ROACH') {
     }
-    
+
+    function mint(address to, Genome genome, uint40[2] calldata parents) external onlyOperator {
+        uint tokenId = roach.length;
+        _mint(to, tokenId);
+        roach[tokenId] = Roach(genome, parents, uint40(block.timestamp), 0);
+    }
+
+    function setGenome(uint tokenId, Genome genome) external onlyOperator {
+        require(_exists(tokenId), "RoachNFT.setGenome: nonexistent token");
+        roach[tokenId].genome = genome;
+    }
+
+    function setBirthCooldown(uint newCooldown) external onlyOwner {
+        BIRTH_COOLDOWN = newCooldown;
+    }
+
+    function canBorn(uint tokenId) external view returns (bool) {
+        return _canBorn(tokenId);
+    }
+
+    function _isGenomeSet(Roach storage r) internal view returns (bool) {
+        return Genome.unwrap(r.genome) != EMPTY_GENOME;
+    }
+
+    function _isBirthColdownPassed(Roach storage r) internal view returns (bool) {
+        return r.creationTime + BIRTH_COOLDOWN <= block.timestamp;
+    }
+
+    function _canBorn(uint tokenId) internal view returns (bool) {
+        Roach storage r = roach[tokenId];
+        return
+            _isGenomeSet(r) &&
+            _isBirthColdownPassed(r);
+    }
+
+    function giveBirth(uint tokenId) external {
+        require(_canBorn(tokenId), 'Still egg');
+        roach[tokenId].birthTime = uint40(block.timestamp);
+        emit Birth(tokenId);
+    }
+
+
 }
