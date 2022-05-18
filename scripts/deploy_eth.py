524e795d548939de8d7b76ec2dfbe19699f6cc3d
kindex <kindex@kindex.lv>
2022-05-19 00:35:21 +0300

    Readme

--- scripts/deploy_eth.py
+++ scripts/deploy_eth.py
@@ -14,8 +14,8 @@ accounts.add(private_key)
 def main():
     print('Deployer account= {}'.format(accounts[0]))
 
-    metadata = Metadata.deploy("https://rrcdevmeta.kindex.lv/meta/roach/v14/",
-                               "https://rrcdevmeta.kindex.lv/meta/contract/v14/",
+    metadata = Metadata.deploy("https://rrcdevmeta.kindex.lv/meta/roach/v15/",
+                               "https://rrcdevmeta.kindex.lv/meta/contract/v15/",
                                {'from':accounts[0]},
                                 publish_source=PUBLISH_SOURCES
     )
@@ -28,6 +28,9 @@ def main():
                                             publish_source=PUBLISH_SOURCES)
     print('genesis_sale = {}'.format(genesis_sale))
     roach_contract.addOperator(genesis_sale, {'from':accounts[0], "required_confs": 0})
-
     genesis_sale.addOperator("0x549E82b2e4831E3d2bCD6dA4a6eBbBf43692D45b", {'from':accounts[0], "required_confs": 0})
 
+    reveal_contract = Reveal.deploy(roach_contract, {'from':accounts[0]},
+                                     publish_source=PUBLISH_SOURCES
+                                     )
+    roach_contract.addOperator(reveal_contract, {'from':accounts[0], "required_confs": 0})
