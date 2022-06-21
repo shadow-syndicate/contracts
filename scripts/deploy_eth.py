2e47eadb1f1040c00d9a1f834826d13f4bfdd51d
kindex <kindex@kindex.lv>
2022-06-21 16:16:55 +0300

    GasStrategy for deploy

--- scripts/deploy_eth.py
+++ scripts/deploy_eth.py
@@ -4,6 +4,7 @@ import logging
 import json
 from brownie import *
 LOGGER = logging.getLogger(__name__)
+from brownie.network.gas.strategies import LinearScalingStrategy
 
 PUBLISH_SOURCES=True
 
@@ -12,22 +13,20 @@ accounts.add(private_key)
 
 def main():
     print('Deployer account= {}'.format(accounts[0]))
+    gas_strategy = LinearScalingStrategy("2 gwei", "2 gwei", 1.1)
 
-    metadata = Metadata.deploy("https://rrcdevmeta.kindex.lv/meta/roach/v18/",
-                               "https://rrcdevmeta.kindex.lv/meta/contract/v18/",
-                               {'from':accounts[0]},
-                                publish_source=PUBLISH_SOURCES
-    )
-    roach_contract = RoachNFT.deploy(metadata, {'from':accounts[0]},
-        publish_source=PUBLISH_SOURCES
-    )
+    metadata = Metadata.deploy("https://rrcdevmeta.kindex.lv/meta/roach/v19/",
+                               "https://rrcdevmeta.kindex.lv/meta/contract/v19/",
+                               {'from':accounts[0], "gas_price": gas_strategy},
+                                publish_source=PUBLISH_SOURCES)
+    roach_contract = RoachNFT.deploy(metadata, {'from':accounts[0], "gas_price": gas_strategy}, publish_source=PUBLISH_SOURCES)
 
-    genesis_sale = GenesisSaleDebug.deploy(roach_contract, round(time.time()), 60*60*24, 10_000,
-                                           {'from':accounts[0]},
+    genesis_sale = GenesisSaleDebug.deploy(roach_contract, round(time.time()), 60*60, 100,
+                                           {'from':accounts[0], "gas_price": gas_strategy},
                                             publish_source=PUBLISH_SOURCES)
     print('genesis_sale = {}'.format(genesis_sale))
-    roach_contract.addOperator(genesis_sale, {'from':accounts[0], "required_confs": 0})
-    genesis_sale.addOperator("0x549E82b2e4831E3d2bCD6dA4a6eBbBf43692D45b", {'from':accounts[0], "required_confs": 0})
+    roach_contract.addOperator(genesis_sale, {'from':accounts[0], "required_confs": 0, "gas_price": gas_strategy})
+    genesis_sale.addOperator("0x549E82b2e4831E3d2bCD6dA4a6eBbBf43692D45b", {'from':accounts[0], "required_confs": 0, "gas_price": gas_strategy})
 
     # reveal_contract = Reveal.deploy(roach_contract, {'from':accounts[0]},
     #                                  publish_source=PUBLISH_SOURCES
